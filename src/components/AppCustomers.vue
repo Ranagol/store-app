cafbe26aec134ef1f8885caeaffb0ad9af48b2b8
Andor <andorhorvat@gmail.com>
2020-03-06 19:27:10 +0100

    Zadatak 2

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <p>Customers.vue</p>
+    <h3>Customers list</h3>
+    <table class="table">
+      <tr>
+        <th>Id</th>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Email</th>
+        <th>Delete</th>
+        <th>Vidi listu proizvoda</th>
+      </tr>
+      <tr v-for="(customer, i) in customers" :key="i">
+        <td>{{ customer.id }}</td>
+        <td>{{ customer.firstName }}</td>
+        <td>{{ customer.lastName }}</td>
+        <td>{{ customer.email }}</td>
+        <td><button @click="deleteCustomer(customer)" class="btn btn-danger">Delete</button></td>
+        <td><button class="btn btn-success">Lista proizvoda</button></td>
+      </tr>
+    </table>
+
   </div>
 </template>
 
@@ -9,8 +28,22 @@
 
 export default {
   name: 'AppCustomers',
+  data(){
+    return {
+      customers: [
+        { id: 1, firstName: 'Miki', lastName: 'Mikic', email: 'miki@gmail.com', listaProizvoda: [], },
+        { id: 2, firstName: 'Ziki', lastName: 'Zikic', email: 'ziki@gmail.com', listaProizvoda: [], },
+      ]
+    }
+  },
   components: {
     
+  },
+  methods: {
+    deleteCustomer(customer){
+      const index = this.customers.indexOf(customer);
+      this.customers.splice(index, 1);
+    }
   }
 }
 </script>
